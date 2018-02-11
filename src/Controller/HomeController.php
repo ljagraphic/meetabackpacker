695aca30bf180af3bd68ad6fc3ac679d97bd9466
ljagraphic <35835045+ljagraphic@users.noreply.github.com>
2018-02-11 21:14:39 +0100

    Install flex tools and create the home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller {
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index() {
+        return $this->render('home.html.twig');
+    }
+
+}
