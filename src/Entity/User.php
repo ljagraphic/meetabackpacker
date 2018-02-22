c77cf21197c9807429495419a2aa2dd5d9c4922e
franzmongin <franz.mongin@gmail.com>
2018-02-22 17:39:33 +0100

    commit de fin de journée

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -294,5 +294,9 @@ class User implements UserInterface, \Serializable {
         $this->createdActivities = $createdActivities;
         return $this;
     }
+    
+    public function __toString() {
+    return $this->name;
+}
 
 }
